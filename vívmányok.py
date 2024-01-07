d2c2e0bd3307298a92273d6dd1c56f7053bec5a6
Hanna <116814006+HannaLarae@users.noreply.github.com>
2024-01-07 22:38:12 +0100

    Add files via upload

---
+++ vívmányok.py
@@ -0,0 +1,7 @@
+f=open("Vívmányok.txt","w". encoding="uft-8")
+k=float(input("Adj meg egy kezdő évszámot: "))
+v=float(input("Adj meg egy végső évszámot: "))
+adatok=[]
+for adat in adatok:
+    print(adat)
+f.close()
